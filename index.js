617f1375883146fa3509535a638a8d2c02525ee0
Yehuda Lehrfield <yll2113@columbia.edu>
2022-11-17 13:31:39 -0500

    feat: dns lookup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -42,49 +43,63 @@ app.get("/api/hello", function (req, res) {
 // url shortener
 app.post("/api/shorturl", (req, res) => {
   let original = req.body.url;
-  // check if the url is already in the database
-  Url.findOne({ original_url: original }, (err, existingUrl) => {
-    if (err) console.log(err);
-    // if in the db, return the doc
-    if (existingUrl) {
-      res.json({
-        original_url: existingUrl.original_url,
-        short_url: existingUrl.short_url,
-      });
-    } else {
-      // url not in the db
-      // check if there are any documents
-      Url.estimatedDocumentCount((err, docCount) => {
+
+  const REMOVE_PROTOCALL = /^https?:\/\//i;
+  const original_mod = original.replace(REMOVE_PROTOCALL, "");
+
+  dns.lookup(original_mod, (err, validAddress) => {
+    // if (err) console.log('Error:' + err);
+    if (err) {
+      console.log("Invalid url");
+      res.json({error:"invalid url"})
+    }
+    else {
+      console.log(validAddress)
+      // check if the url is already in the database
+      Url.findOne({ original_url: original }, (err, existingUrl) => {
         if (err) console.log(err);
-        // if there are no documents
-        if (docCount == 0) {
-          //create one with short_url = 1
-          Url.create({
-            original_url: original,
-            short_url: 1,
+        // if in the db, return the doc
+        if (existingUrl) {
+          res.json({
+            original_url: existingUrl.original_url,
+            short_url: existingUrl.short_url,
           });
         } else {
-          // documents exists
-          // get largest shorturl and create a new document with a shorturl (incremented)
-          Url.findOne({})
-            .sort({ short_url: "desc" })
-            .exec((err, lastDoc) => {
-              if (err) console.log(err);
-              const shortUrl = lastDoc.short_url + 1;
-              // add new document and return the json
-              Url.findOneAndUpdate(
-                { original_url: original },
-                { short_url: shortUrl },
-                { returnDocument: "after", upsert: true },
-                (err, doc) => {
+          // url not in the db
+          // check if there are any documents
+          Url.estimatedDocumentCount((err, docCount) => {
+            if (err) console.log(err);
+            // if there are no documents
+            if (docCount == 0) {
+              //create one with short_url = 1
+              Url.create({
+                original_url: original,
+                short_url: 1,
+              });
+            } else {
+              // documents exists
+              // get largest shorturl and create a new document with a shorturl (incremented)
+              Url.findOne({})
+                .sort({ short_url: "desc" })
+                .exec((err, lastDoc) => {
                   if (err) console.log(err);
-                  res.json({
-                    original_url: doc.original_url,
-                    short_url: doc.short_url,
-                  });
-                }
-              );
-            });
+                  const shortUrl = lastDoc.short_url + 1;
+                  // add new document and return the json
+                  Url.findOneAndUpdate(
+                    { original_url: original },
+                    { short_url: shortUrl },
+                    { returnDocument: "after", upsert: true },
+                    (err, doc) => {
+                      if (err) console.log(err);
+                      res.json({
+                        original_url: doc.original_url,
+                        short_url: doc.short_url,
+                      });
+                    }
+                  );
+                });
+            }
+          });
         }
       });
     }
